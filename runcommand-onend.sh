476503250cb9e1b4ac67e8359ff0536196dc5a34
AR Crypto <37816162+ALLRiPPED@users.noreply.github.com>
2022-03-04 01:15:09 -0600

    Update runcommand-onend.sh

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # /etc/init.d/start-sound
 
-sudo omxplayer --vol 500 --amp 250 -b /home/pi/RetroPie/splashscreens/ThanksForPlaying.mp4 > /dev/null 2>&1
+sudo omxplayer --vol 250 --amp 250 -b /home/pi/RetroPie/splashscreens/ThanksForPlaying.mp4 > /dev/null 2>&1
