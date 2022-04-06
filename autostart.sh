d9faef4b38b0790fbb9f726c1e9548396ca74ebb
AR Crypto <37816162+ALLRiPPED@users.noreply.github.com>
2022-04-06 17:13:13 -0500

    more work on v1.20

--- autostart.sh
+++
@@ -1,3 +0,0 @@
-(nohup python /home/pi/.tampo/BGM.py > /dev/null 2>&1) &
-emulationstation #auto
-/opt/retropie/configs/all/emulationstation/scripts/shutdown/exit-splash
--- runcommand-onend.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-# /etc/init.d/start-sound
-
-sudo omxplayer --vol 250 --amp 250 -b /home/pi/RetroPie/splashscreens/ThanksForPlaying.mp4 > /dev/null 2>&1
